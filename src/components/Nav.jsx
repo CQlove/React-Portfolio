b3a6439a7d2d89aea010339b81fc7ac015740765
Jianxiong Chen <chenjianxiong1997@gmail.com>
2023-09-22 17:22:42 -0600

    added some files

---
+++ src/components/Nav.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+const Nav = () => {
+    return (
+        <ul className="nav nav-pills nav-fill">
+            <li className="nav-item">
+                <Link
+                    to="/"
+                    // This is a conditional (ternary) operator that checks to see if the current page is "Home"
+                    // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
+                    className={currentPage === '/' ? 'nav-link active' : 'nav-link'}
+                >
+                    About Me
+                </Link>
+            </li>
+            <li className="nav-item">
+                <Link
+                    to="/Portfolio"
+                    // Check to see if the currentPage is `Portfolio`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+                    className={currentPage === '/Portfolio' ? 'nav-link active' : 'nav-link'}
+                >
+                    Portfolio
+                </Link>
+            </li>
+            <li className="nav-item">
+                <Link
+                    to="/Contact"
+                    // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+                    className={currentPage === '/Contact' ? 'nav-link active' : 'nav-link'}
+                >
+                    Contact
+                </Link>
+            </li>
+            <li className="nav-item">
+                <Link
+                    to="/Resume"
+                    // Check to see if the currentPage is `Resume`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+                    className={currentPage === '/Resume' ? 'nav-link active' : 'nav-link'}
+                >
+                    Resume
+                </Link>
+            </li>
+        </ul>
+    );
+}
+
+export default Nav;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,42 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App.jsx';
+import Error from './pages/Error.jsx';
+import Home from './pages/Home.jsx';
+import Portfolio from './pages/Portfolio.jsx';;
+import Contact from './pages/Contact.jsx';
+import Resume from './pages/Resume.jsx';
+
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: '/Portfolio',
+        element: <Portfolio />,
+      },
+      {
+        path: '/Contact',
+        element: <Contact />,
+      },
+      {
+        path: '/Resume',
+        element: <Resume />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )
