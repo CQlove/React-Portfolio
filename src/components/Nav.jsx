42d53397a276f327846a9e3c8e200d571bb17673
Jianxiong Chen <chenjianxiong1997@gmail.com>
2023-09-22 17:32:22 -0600

    export pages and let nav bar work

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,15 @@
 import React from "react";
+import { Link, useLocation } from 'react-router-dom';
 
 const Nav = () => {
+    const currentPage = useLocation().pathname;
     return (
         <ul className="nav nav-pills nav-fill">
             <li className="nav-item">
                 <Link
                     to="/"
                     // This is a conditional (ternary) operator that checks to see if the current page is "Home"
+                    // Home page is the About Me page
                     // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
                     className={currentPage === '/' ? 'nav-link active' : 'nav-link'}
                 >
