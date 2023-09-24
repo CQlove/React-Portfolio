8b10cd01767e2318a882467e94ff35ecc55077be
Jianxiong Chen <chenjianxiong1997@gmail.com>
2023-09-24 04:45:15 -0600

    added style for header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,13 +3,14 @@ import { useLocation } from 'react-router-dom';
 import JC from '../assets/JC.jpg';
 import Nav from "./Nav";
 import '../styles/Header.css';
+import '../styles/Nav.css';
 
 const Headers = () => {
     const currentPage = useLocation().pathname;
     return (
         <div id="header" class="container-fluid" style={{ backgroundColor: '#8D9092' }}>
-            <div class="row">
-                <div class="col-lg-1 col-md-1 col-sm-1">
+            <div id="row-col" class="row">
+                <div class="col-xl-1 col-lg-1 col-md-1 col-sm-1">
                     <img
                         id="jc"
                         src={JC}
@@ -17,9 +18,9 @@ const Headers = () => {
                         height="50"
                     />
                 </div>
-                <div class="col-lg-5 col-md-4 col-sm-1">
+                <div class="col-xl-4 col-lg-3 col-md-1">
                 </div>
-                <div class="col-lg-6 col-md-7 col-sm-10">
+                <div class="col-xl-7 col-lg-8 col-md-10">
                     <div>
                         <Nav currentPage={currentPage} />
                     </div>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,48 +1,94 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from 'react-router-dom';
 
 const Nav = ({ currentPage }) => {
+    const [activeLink, setActiveLink] = useState('/');
+    const handleLinkClick = (linkName) => {
+        setActiveLink(linkName);
+    };
     return (
-        <ul className="nav nav-pills nav-fill" >
-            <li className="nav-item">
-                <Link
-                    to="/"
-                    // This is a conditional (ternary) operator that checks to see if the current page is "Home"
-                    // Home page is the About Me page
-                    // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
-                    className={currentPage === '/' ? 'nav-link active' : 'nav-link'}
-                >
-                    About Me
-                </Link>
-            </li>
-            <li className="nav-item">
-                <Link
-                    to="/Portfolio"
-                    // Check to see if the currentPage is `Portfolio`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-                    className={currentPage === '/Portfolio' ? 'nav-link active' : 'nav-link'}
-                >
-                    Portfolio
-                </Link>
-            </li>
-            <li className="nav-item">
-                <Link
-                    to="/Contact"
-                    // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-                    className={currentPage === '/Contact' ? 'nav-link active' : 'nav-link'}
-                >
-                    Contact
-                </Link>
-            </li>
-            <li className="nav-item">
-                <Link
-                    to="/Resume"
-                    // Check to see if the currentPage is `Resume`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-                    className={currentPage === '/Resume' ? 'nav-link active' : 'nav-link'}
-                >
-                    Resume
-                </Link>
-            </li>
-        </ul>
+        // <nav>
+        //     <ul className="nav nav-pills nav-fill" aria-orientation="vertical">
+        //         <li className="nav-item">
+        //             <Link
+        //                 to="/"
+        //                 // This is a conditional (ternary) operator that checks to see if the current page is "Home"
+        //                 // Home page is the About Me page
+        //                 // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
+        //                 className={currentPage === '/' ? 'nav-link active' : 'nav-link'}
+        //             >
+        //                 About Me
+        //             </Link>
+        //         </li>
+        //         <li className="nav-item">
+        //             <Link
+        //                 to="/Portfolio"
+        //                 // Check to see if the currentPage is `Portfolio`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+        //                 className={currentPage === '/Portfolio' ? 'nav-link active' : 'nav-link'}
+        //             >
+        //                 Portfolio
+        //             </Link>
+        //         </li>
+        //         <li className="nav-item">
+        //             <Link
+        //                 to="/Contact"
+        //                 // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+        //                 className={currentPage === '/Contact' ? 'nav-link active' : 'nav-link'}
+        //             >
+        //                 Contact
+        //             </Link>
+        //         </li>
+        //         <li className="nav-item">
+        //             <Link
+        //                 to="/Resume"
+        //                 // Check to see if the currentPage is `Resume`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+        //                 className={currentPage === '/Resume' ? 'nav-link active' : 'nav-link'}
+        //             >
+        //                 Resume
+        //             </Link>
+        //         </li>
+        //     </ul>
+        // </nav>
+        <nav>
+            <ul>
+                <li>
+                    <Link
+                        to="/"
+                        className={activeLink === '/' ? 'active' : ''}
+                        onClick={() => handleLinkClick('/')}
+                    >
+                        About Me
+                    </Link>
+                </li>
+                <li>
+                    <Link
+                        to="/Portfolio"
+                        className={activeLink === '/Portfolio' ? 'active' : ''}
+                        onClick={() => handleLinkClick('/Portfolio')}
+                    >
+                        Portfolio
+                    </Link>
+                </li>
+                <li>
+                    <Link
+                        to="/Contact"
+                        className={activeLink === '/Contact' ? 'active' : ''}
+                        onClick={() => handleLinkClick('/Contact')}
+                    >
+                        Contact
+                    </Link>
+                </li>
+                <li>
+                    <Link
+                        to="/Resume"
+                        className={activeLink === '/Resume' ? 'active' : ''}
+                        onClick={() => handleLinkClick('/Resume')}
+                    >
+                        Resume
+                    </Link>
+                </li>
+            </ul>
+        </nav>
     );
 }
 
