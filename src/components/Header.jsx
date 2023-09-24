358a6e3705e2824b5a8eb1a8629dac238c2822e2
Jianxiong Chen <chenjianxiong1997@gmail.com>
2023-09-24 02:59:20 -0600

    added logo animation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,14 +2,17 @@ import React from "react";
 import { useLocation } from 'react-router-dom';
 import JC from '../assets/JC.jpg';
 import Nav from "./Nav";
+import '../styles/Header.css';
 
 const Headers = () => {
     const currentPage = useLocation().pathname;
     return (
-        <div class="container-fluid" style={{ backgroundColor: '#8D9092' }}>
+        <div id="header" class="container-fluid" style={{ backgroundColor: '#8D9092' }}>
             <div class="row">
                 <div class="col-lg-1 col-md-1 col-sm-1">
-                    <img src={JC}
+                    <img
+                        id="jc"
+                        src={JC}
                         width="50"
                         height="50"
                     />
@@ -17,7 +20,10 @@ const Headers = () => {
                 <div class="col-lg-5 col-md-4 col-sm-1">
                 </div>
                 <div class="col-lg-6 col-md-7 col-sm-10">
-                    <Nav currentPage={currentPage} />
+                    <div>
+                        <Nav currentPage={currentPage} />
+                    </div>
+
                 </div>
             </div>
         </div >
