b8af82363a57f6fb5ec48863c82c80171d6f07d7
Jianxiong Chen <chenjianxiong1997@gmail.com>
2023-09-24 05:26:37 -0600

    changed style for header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,26 +8,15 @@ import '../styles/Nav.css';
 const Headers = () => {
     const currentPage = useLocation().pathname;
     return (
-        <div id="header" class="container-fluid" style={{ backgroundColor: '#8D9092' }}>
-            <div id="row-col" class="row">
-                <div class="col-xl-1 col-lg-1 col-md-1 col-sm-1">
-                    <img
-                        id="jc"
-                        src={JC}
-                        width="50"
-                        height="50"
-                    />
-                </div>
-                <div class="col-xl-4 col-lg-3 col-md-1">
-                </div>
-                <div class="col-xl-7 col-lg-8 col-md-10">
-                    <div>
-                        <Nav currentPage={currentPage} />
-                    </div>
-
-                </div>
-            </div>
-        </div >
+        <header id="header" style={{ backgroundColor: '#8D9092' }}>
+            <img
+                id="jc"
+                src={JC}
+                width="50"
+                height="50"
+            />
+            <Nav currentPage={currentPage} />
+        </header >
     );
 }
 
